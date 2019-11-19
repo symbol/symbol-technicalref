28606062414e25604d7ac7deba156618b242b052
gimer <gimre@127.0.0.1>
2019-11-19 14:13:33 +0100

    index generation

--- whitepaper/compile.bat
+++ whitepaper/compile.bat
@@ -1,6 +1,7 @@
 set BUILDDIR=..\_build
 set COMPILER=pdflatex
 set BIBER=biber
+set MAKEIDX=makeindex
 
 set PROJECT=main
 set BIBLIOGRAPHY=bibliography
@@ -10,18 +11,23 @@ set BIBLIOGRAPHY=bibliography
 @mkdir %BUILDDIR%\chapters >NUL
 @mkdir %BUILDDIR%\header >NUL
 
-@%COMPILER% -interaction=nonstopmode -output-directory="%BUILDDIR%" "%PROJECT%.tex" >NUL
+@%COMPILER% -interaction=nonstopmode -halt-on-error -output-directory="%BUILDDIR%" "%PROJECT%.tex" >NUL
 @echo "First pass (via %COMPILER%) done!"
 
 @copy /Y %BIBLIOGRAPHY%.bib %BUILDDIR%
 @%BIBER% --output_directory=%BUILDDIR% %PROJECT% >NUL
 @echo "Second pass (via %BIBER%) done!"
 
-@%COMPILER% -interaction=nonstopmode -output-directory=%BUILDDIR% %PROJECT%.tex >NUL
-@echo "Third pass (via %COMPILER%) done!"
+@%MAKEIDX% %BUILDDIR%\main.idx -o main.ind -t main.ilg
+@move /Y main.ind %BUILDDIR%
+@move /Y main.ilg %BUILDDIR%
+@echo "Third pass (via %MAKEIDX%) done!"
 
 @%COMPILER% -interaction=nonstopmode -halt-on-error -output-directory=%BUILDDIR% %PROJECT%.tex >NUL
 @echo "Fourth pass (via %COMPILER%) done!"
 
-@echo "Compilation done. Output file can be seen in %BUILDDIR)"
+@%COMPILER% -interaction=nonstopmode -halt-on-error -output-directory=%BUILDDIR% %PROJECT%.tex >NUL
+@echo "Fifth pass (via %COMPILER%) done!"
+
+@echo "Compilation done. Output file can be seen in %BUILDDIR%"
 
